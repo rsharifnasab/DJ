be51479bb36403c1fd6e5e054f86a08cd6d9e820
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-07-01 20:33:42 +0430

    output and return val is ok at the same time

--- pkg/student/main.go
+++ pkg/student/main.go
@@ -1,63 +1,90 @@
 package student
 
 import (
-	"bufio"
 	"context"
 	"fmt"
-	"io"
-	"os"
+	"io/ioutil"
 	"os/exec"
 	"strings"
 	"time"
 )
 
 func Run() {
-	const submissionFile = "/tmp/dj/a.cpp"
+	const submissionFile = "./examples/solution.cpp"
 	_ = submissionFile
 	// todo compile
 
-	const compiledFile = "/tmp/dj/a.out"
+	const compiledFile = "./examples/a.out"
 
 	const timeout = 2 * time.Second
 
-	for i := 1; i <= 4; i++ {
-		testAddr := fmt.Sprintf("/tmp/dj/tests/in/input%d.txt", i)
-		rawFile, err := os.Open(testAddr)
+	for i := 1; i <= 5; i++ {
+		testInpAddr := fmt.Sprintf("./examples/tests/in/input%d.txt", i)
+		testInpData, err := ioutil.ReadFile(testInpAddr)
 		if err != nil {
 			panic(err)
 		}
+		println("test input : [", strings.TrimSpace(string(testInpData)), "]")
 
-		bufReader := bufio.NewReader(rawFile)
-
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		defer cancel() // cleanup resources
+		ctx, closeContext := context.WithTimeout(context.Background(), timeout)
+		defer closeContext() // cleanup resources
 
 		// Create the command with our context
 		cmd := exec.CommandContext(ctx, compiledFile)
 
-		stdin, err := cmd.StdinPipe()
+		stdinWriter, err := cmd.StdinPipe()
+		stdinWriter.Write(testInpData)
 		if err != nil {
 			panic(err)
 		}
-		stdinBuf := bufio.NewWriter(stdin)
-		stdinBuf.ReadFrom(bufReader)
-		if _, err = io.WriteString(stdin, "0\n"); err != nil {
-			panic(err)
-		}
 
-		err = cmd.Run()
+		out, err := cmd.Output()
+		//err = cmd.Run()
+		if ctx.Err() != nil {
+			println(ctx.Err().Error())
+		}
+		//cmd.Wait()
 
 		if ctx.Err() == context.DeadlineExceeded {
-			fmt.Println("time limit")
-			continue
+			fmt.Println("time limit exceed")
+		} else {
+			outStr := strings.TrimSpace(string(out))
+			fmt.Printf("Output: [%v]\n", outStr)
 		}
-
-		out, _ := cmd.Output()
-		outStr := strings.TrimSpace(string(out))
-		fmt.Printf("Output: [%v]\n", outStr)
 
 		if err != nil {
-			fmt.Println("Non-zero exit code or program closed", err)
+			fmt.Printf("abnormal status : [%v]\n", err)
+		} else {
+			fmt.Println("normal exit")
 		}
+
+		println()
 	}
 }
+
+/*
+
+	direct write to stdin of program
+	if _, err = io.WriteString(stdin, "0\n"); err != nil {
+		panic(err)
+	}
+
+
+	pipe a file to stdin
+
+	rawFile, err := os.Open(testAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	bufReader := bufio.NewReader(rawFile)
+
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	stdinBuf := bufio.NewWriter(stdin)
+	stdinBuf.ReadFrom(bufReader)
+
+*/
