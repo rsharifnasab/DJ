234f49e423f0ab6dc6e5d24b95cf143188112df5
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-07-02 11:58:37 +0430

    one more example

--- examples/solution.cpp
+++ examples/solution.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+void f()
+{
+    f();
+}
 int main()
 {
     int i;
     std::cin >> i;
-    int *p = NULL;
+    int* p = NULL;
     switch (i) {
-        case 1: // time limit 
-            while(1)
-                ;
-            break;
-        case 2: // non-zero exit code
-            return 1;
-        case 3: // runtime error
-            return 1/0;
-        case 4: // runtime error again
-            return *(p);
-        default: // ok
-            std::cout << "input : " << i << std::endl;
+    case 1: // time limit
+        while (1)
+            ;
+        break;
+    case 2: // non-zero exit code
+        return 1;
+    case 3: // runtime error
+        return 1 / 0;
+    case 4: // runtime error again
+        return *(p);
+    case 5:
+        f();
+    default: // ok
+        std::cout << "input : " << i << std::endl;
     }
     return 0;
 }
